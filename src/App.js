4b7f31fab1985aebe624aba9e8a52def3c6b6ffe
apta-tamilarasi <athinarayanantamilarasi@gamil.com>
2023-11-22 13:34:13 +0530

    addnew

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Function from './Component/Function.js';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Function/>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/Component/Function.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+const Function=()=>{
+    return <div>
+        <h1>Function Component</h1>
+    </div>
+}
+export default Function
